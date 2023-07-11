6665d9b4c58ddf68fa6a84358c30ca8c3a913a0b
yevenes1 <yevenesdev@gmail.com>
2023-07-11 14:01:06 -0400

    fix js values

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
-function calcularEdad() {
-    let nombre = prompt("Ingrese su nombre:");
-    let añoNacimiento = prompt("Ingrese su año de nacimiento:");
-    
-    const fechaActual = new Date();
-    const añoActual = fechaActual.getFullYear();
-    
-    let edad = añoActual - añoNacimiento;
-  
-    let resultado = `Hola, ${nombre}. Tienes ${edad} años.`;
-    alert(resultado);
+function realizarCalculo(operacion) {
+  let num1 = Number(prompt("Ingrese el primer número:"));
+  let num2 = Number(prompt("Ingrese el segundo número:"));
+
+  let resultado;
+
+  switch (operacion) {
+    case 'suma':
+      resultado = num1 + num2;
+      break;
+    case 'resta':
+      resultado = num1 - num2;
+      break;
+    case 'concatenar':
+      resultado = num1.toString() + num2.toString();
+      break;
+    case 'division':
+      resultado = num1 / num2;
+      break;
+    case 'porcentaje':
+      resultado = (num1 * num2) / 100;
+      break;
+    default:
+      alert("Operación no válida.");
+      return;
   }
-  
+
+  alert(`El resultado de la operación "${operacion}" es: ${resultado}`);
+}
